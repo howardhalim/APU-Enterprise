6ed083091e889aa93ca5086d26f56667579c5955
Steven <57295193+sterbeN27@users.noreply.github.com>
2022-03-05 21:26:08 +0800

    Update Server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -51,7 +51,7 @@ public class Server implements Interface {
         try
         {
             //change to appropriate directory
-            String url = "jdbc:sqlite:D:\\DCOMS\\APU-Enterprise\\APUDatabase.db";
+            String url = "jdbc:sqlite:APUDatabase.db";
             con = DriverManager.getConnection(url);
 
         }
